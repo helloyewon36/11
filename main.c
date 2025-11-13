1b4a4d703101d799626486604f94bf6286e4afe2
helloyewon36 <helloyewon36@gmail.com>
2025-11-13 15:46:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -11,19 +11,23 @@ int main(int argc, char *argv[])
     int grade[5];
     int sum=0;
     
-    for (i=o;i<5;i++)
+    for (i=0;i<5;i++)
     {
         printf("grade[%i]= ", i);
         scanf("%d", &grade[i]);
         
-    for (i=o;i<5,i++)
+    for (i=0;i<5;i++)
     {
         printf("grade[%i] = %i\n", i, *(grade+i));
         sum=sum + *(grade+i);
         }
         
-    printf("Average  : %i\n", sum(5);
+    printf("Average  : %i\n", sum/5);
   
   system("PAUSE");	
   return 0;
 }
+
+
+
+

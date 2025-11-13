ef6ad29121733d17f795140ac6c6f16877e34862
helloyewon36 <helloyewon36@gmail.com>
2025-11-13 15:22:59 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i=4;
+    
+    int *pi=&i;
+    char *pc=&i;
+    
+    printf("%i, %i, %i\n", i, *pi, *pc);
+    
+  
+  system("PAUSE");	
+  return 0;
+}
